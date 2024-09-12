4a339899b542e01ec67d2301d858c513bb76d69f
Uğurtan Pektaş <ugurtanpektas@Ugurtan-MacBook-Pro.local>
2024-09-12 17:48:27 +0300

    Table changed with grid for react-window list

--- src/components/BetRow.js
+++ src/components/BetRow.js
@@ -1,82 +1,79 @@
 /* eslint-disable react/prop-types */
-import React, { useState, memo } from 'react';
+import React, { memo } from 'react';
 import { useBet } from '../contexts/BetContext';
 
 const BetRow = memo(({ bet }) => {
     const { addOrUpdateBet, checkBetOdd } = useBet();
-    const [selectedOdd, setSelectedOdd] = useState(null);
 
     const handleClick = (oddKey, oddValue) => {
         const isSelected = checkBetOdd(bet.NID, oddKey);
 
         if (isSelected) {
-            setSelectedOdd(null);
             addOrUpdateBet(bet, '', oddKey);
         } else {
-            setSelectedOdd(oddKey);
             addOrUpdateBet(bet, oddValue, oddKey);
         }
     };
 
     return (
         <>
-            <tr className="first-line">
-                <td className="ln">{bet.D} {bet.DAY} {bet.LN}</td>
-                <td>Yorumlar</td>
-                <td>MBS</td>
-                <td>1</td>
-                <td>x</td>
-                <td>2</td>
-                <td>Alt</td>
-                <td>Üst</td>
-                <td>H1</td>
-                <td>1</td>
-                <td>x</td>
-                <td>2</td>
-                <td>H2</td>
-                <td>1-x</td>
-                <td>1-2</td>
-                <td>x-2</td>
-                <td>Var</td>
-                <td>Yok</td>
-                <td>+99</td>
-            </tr>
-            <tr>
-                <td className="ln"><b>{bet.C}</b> {bet.T} {bet.N}</td>
-                <td></td>
-                <td>{bet.OCG[1]?.MBS}</td>
-                <td
-                    className={`pointer ${selectedOdd === '1' ? 'yellow' : ''}`}
+            <div className="first-line grid-row">
+                <div className="ln">{bet.D} {bet.DAY} {bet.LN}</div>
+                <div>Yorumlar</div>
+                <div>MBS</div>
+                <div>1</div>
+                <div>x</div>
+                <div>2</div>
+                <div>Alt</div>
+                <div>Üst</div>
+                <div>H1</div>
+                <div>1</div>
+                <div>x</div>
+                <div>2</div>
+                <div>H2</div>
+                <div>1-x</div>
+                <div>1-2</div>
+                <div>x-2</div>
+                <div>Var</div>
+                <div>Yok</div>
+                <div>+99</div>
+            </div>
+            <div className='grid-row'>
+                <div className="ln"><b>{bet.C}</b> {bet.T} {bet.N}</div>
+                <div></div>
+                <div>{bet.OCG[1]?.MBS}</div>
+                <div
+                    className={`pointer ${checkBetOdd(bet.NID, '1') ? 'yellow' : ''}`}
                     onClick={() => handleClick('1', bet.OCG[1]?.OC[0]?.O || '')}
                 >
                     {bet.OCG[1]?.OC[0]?.O || ''}
-                </td>
-                <td
-                    className={`pointer ${selectedOdd === 'x' ? 'yellow' : ''}`}
+                </div>
+                <div
+                    className={`pointer ${checkBetOdd(bet.NID, 'x') ? 'yellow' : ''}`}
                     onClick={() => handleClick('x', bet.OCG[1]?.OC[1]?.O || '')}
                 >
                     {bet.OCG[1]?.OC[1]?.O || ''}
-                </td>
-                <td></td>
-                <td className={`pointer ${selectedOdd === 'alt' ? 'yellow' : ''}`}
-                    onClick={() => handleClick('alt', bet.OCG[5]?.OC[25]?.O || '')}>{bet.OCG[5]?.OC[25]?.O || ''}</td>
-                <td className={`pointer ${selectedOdd === 'ust' ? 'yellow' : ''}`}
-                    onClick={() => handleClick('ust', bet.OCG[5]?.OC[26]?.O || '')}>{bet.OCG[5]?.OC[26]?.O || ''}</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td className={`pointer ${selectedOdd === '1-x' ? 'yellow' : ''}`}
-                    onClick={() => handleClick('1-x', bet.OCG[2]?.OC[3]?.O || '')}>{bet.OCG[2]?.OC[3]?.O || ''}</td>
-                <td className={`pointer ${selectedOdd === '1-2' ? 'yellow' : ''}`}
-                    onClick={() => handleClick('1-2', bet.OCG[2]?.OC[4]?.O || '')}>{bet.OCG[2]?.OC[4]?.O || ''}</td>
-                <td className={`pointer ${selectedOdd === 'x-2' ? 'yellow' : ''}`}
-                    onClick={() => handleClick('x-2', bet.OCG[2]?.OC[5]?.O || '')}>{bet.OCG[2]?.OC[5]?.O || ''}</td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+                </div>
+                <div></div>
+                <div className={`pointer ${checkBetOdd(bet.NID, 'alt') ? 'yellow' : ''}`}
+                    onClick={() => handleClick('alt', bet.OCG[5]?.OC[25]?.O || '')}>{bet.OCG[5]?.OC[25]?.O || ''}</div>
+                <div className={`pointer ${checkBetOdd(bet.NID, 'ust') ? 'yellow' : ''}`}
+                    onClick={() => handleClick('ust', bet.OCG[5]?.OC[26]?.O || '')}>{bet.OCG[5]?.OC[26]?.O || ''}</div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div className={`pointer ${checkBetOdd(bet.NID, '1-x') ? 'yellow' : ''}`}
+                    onClick={() => handleClick('1-x', bet.OCG[2]?.OC[3]?.O || '')}>{bet.OCG[2]?.OC[3]?.O || ''}</div>
+                <div className={`pointer ${checkBetOdd(bet.NID, '1-2') ? 'yellow' : ''}`}
+                    onClick={() => handleClick('1-2', bet.OCG[2]?.OC[4]?.O || '')}>{bet.OCG[2]?.OC[4]?.O || ''}</div>
+                <div className={`pointer ${checkBetOdd(bet.NID, 'x-2') ? 'yellow' : ''}`}
+                    onClick={() => handleClick('x-2', bet.OCG[2]?.OC[5]?.O || '')}>{bet.OCG[2]?.OC[5]?.O || ''}</div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </>
     );
 });
--- src/components/BetsTable.js
+++ src/components/BetsTable.js
@@ -1,40 +1,49 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { FixedSizeList as List } from 'react-window';
 import BetRow from './BetRow';
 
 function BetsTable({ bets }) {
+    const Row = ({ index, style }) => (
+        <div style={style}>
+            <BetRow bet={bets[index]} />
+        </div>
+    );
+
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th className="ln">Events: {bets.length}</th>
-                    <th>Yorumlar</th>
-                    <th>MBS</th>
-                    <th>1</th>
-                    <th>x</th>
-                    <th>2</th>
-                    <th>Alt</th>
-                    <th>Üst</th>
-                    <th>H1</th>
-                    <th>1</th>
-                    <th>x</th>
-                    <th>2</th>
-                    <th>H2</th>
-                    <th>1-x</th>
-                    <th>1-2</th>
-                    <th>x-2</th>
-                    <th>Var</th>
-                    <th>Yok</th>
-                    <th>+99</th>
-                </tr>
-            </thead>
-            <tbody>
-                {bets.map((bet) => (
-                    <BetRow key={bet.NID} bet={bet} />
-                ))}
-            </tbody>
-        </table>
+        <div>
+            <div className="header grid-row">
+                <div className="ln">Events: {bets.length}</div>
+                <div>Yorumlar</div>
+                <div>MBS</div>
+                <div>1</div>
+                <div>x</div>
+                <div>2</div>
+                <div>Alt</div>
+                <div>Üst</div>
+                <div>H1</div>
+                <div>1</div>
+                <div>x</div>
+                <div>2</div>
+                <div>H2</div>
+                <div>1-x</div>
+                <div>1-2</div>
+                <div>x-2</div>
+                <div>Var</div>
+                <div>Yok</div>
+                <div>+99</div>
+            </div>
+            <List
+                height={window.innerHeight - 60} // Set height according to your design
+                itemCount={bets.length}
+                itemSize={71}
+                width="100%"
+            >
+                {Row}
+            </List>
+        </div>
     );
 }
 
 export default BetsTable;
+
